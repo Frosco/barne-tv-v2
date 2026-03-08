5c1dbb94f47a8edc0365300302d309c6f25db2b5
Nils Refsnes <23233709+Frosco@users.noreply.github.com>
2026-03-08 14:30:31 +0100

    feat: wire up main.go entry point

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "path to config file")
+	addr := flag.String("addr", ":8080", "listen address")
+	flag.Parse()
+
+	cfg, err := LoadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("loading config: %v", err)
+	}
+
+	yt := NewYouTubeClient(cfg.YouTubeAPIKey)
+
+	cache := &VideoCache{}
+	if err := cache.RefreshAll(yt, cfg.Sources); err != nil {
+		log.Fatalf("initial refresh: %v", err)
+	}
+
+	interval, err := time.ParseDuration(cfg.RefreshInterval)
+	if err != nil {
+		log.Fatalf("invalid refresh_interval %q: %v", cfg.RefreshInterval, err)
+	}
+	stop := cache.StartPeriodicRefresh(yt, cfg.Sources, interval)
+	defer stop()
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatalf("parsing template: %v", err)
+	}
+
+	handler := &GridHandler{Cache: cache, Template: tmpl, GridSize: 9}
+
+	http.Handle("/", handler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Printf("listening on %s with %d sources", *addr, len(cfg.Sources))
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}

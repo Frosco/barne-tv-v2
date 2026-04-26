12914389e26b43104d57142bba08170f7f4a6967
Nils Refsnes <23233709+Frosco@users.noreply.github.com>
2026-04-26 22:37:35 +0200

    feat: expand video grid from 4x3 to 6x5 (18 to 30 videos)

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 		log.Fatalf("parsing template: %v", err)
 	}
 
-	handler := &GridHandler{Cache: cache, Template: tmpl, GridSize: 18}
+	handler := &GridHandler{Cache: cache, Template: tmpl, GridSize: 30}
 
 	http.Handle("/", handler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
